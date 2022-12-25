773a484865e4ad4658954d9c3c1d6db81923b146
Suraj Kumar <surajgupta6205@gmail.com>
2022-12-25 16:45:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Header from "./components/Header";
+import OptionsTab from "./components/OptionsTab";
+import Container from "@mui/material/Container";
+import LocationCards from "./components/LocationCards";
+import Footer from "./components/Footer";
+import FooterMenu from "./components/FooterMenu";
+import {displayOnDesktop} from './themes/commonStyles'
+import MobileFooter from "./components/MobileFooter";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          height: "100vh",
+        }}
+      >
+        <Box>
+          <Header />
+          <OptionsTab />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              flexGrow: 1,
+              height: 360,
+              overflowY: "scroll",
+            }}
+          >
+            <Container maxWidth="xl" sx={{ mb: 3 }}>
+              <LocationCards />
+              <Box sx={{
+                display: {xs: 'flex', md: 'none'}
+              }}>
+                <MobileFooter />
+              </Box>
+            </Container>
+          </Box>
+        </Box>
+        <Box sx={{ display: {xs: 'flex', md: 'none'}}}>
+          <FooterMenu />
+        </Box>
+        <Box sx={displayOnDesktop}>
+          <Footer />
+        </Box>
+      </Box>
+    </React.Fragment>
   );
 }
 
